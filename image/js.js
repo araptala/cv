f925592317c859be294217bd4dd959753141edde
araptala <121856163+araptala@users.noreply.github.com>
2023-02-15 16:59:30 +0300

    Add files via upload
    V1.2 added new photography section page

---
+++ image/js.js
@@ -0,0 +1,15 @@
+const result = document.querySelector("#result");
+result.style.backgroundColor = "blue";
+
+const item = document.querySelector(".special");
+// console.log(item);
+
+const lastItem = document.querySelector("li:last-child");
+// console.log(lastItem);
+
+const list = document.querySelectorAll(".special");
+
+list.forEach(function (item) {
+  console.log(item);
+  item.style.color = "yellow";
+});
